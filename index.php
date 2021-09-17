3a394dd5a4232f227a523031a954b5095fd7ef73
santosriguete <89820835+santosriguete@users.noreply.github.com>
2021-09-16 22:39:46 -0300

    Aula do dia 16/09
    Início da programação da Agenda baseada em array.

---
+++ index.php
@@ -0,0 +1,64 @@
+<DOCTYPE html>
+<html lang="pt-br">
+    <head> 
+        <meta charset="utf=8"/>
+        <meta http-equiv="content-language" content="pt=br"/> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Minha Agenda</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+        <header> <!--CABEÇALHO DO SITE -->
+            <img src="imagens/logo_contatos.png" width="25%" height="20%"/>
+            <nav class="navbar navbar-expand-lg navbar-ligth bg-ligth"> <!-- COLOCO O MENU DO SITE -->
+                <div class="container-fluid">
+                    <h3>Contatos</h3>
+                    <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Cadastrar Contatos</button> <!-- COLOCO BOTÃO DO SITE -->
+                </div>
+            </nav>
+        </header>
+        <div class="container-fluid">
+            <section>
+                <br/>
+                <div class="row justify-content-center row-cols-2 row-cols-md-1 mb-3 text-center">
+                    <div class="col">
+                        <div class="card mb-4 rounded-3 shadow-sm">
+                            <div class="card-hearder py-3">
+                                <h4 class="my-0 fw-normal"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+                                </svg>&nbsp;<b>Meus Contatos</b><h4>
+                            </div>
+                            <div class="card-body">
+                                <?php
+                                    include 'lista.php';
+                                ?>
+                            </div>
+                        </div>
+                </div>
+            </section>
+
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Teste de Modal
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <footer>
+            <hr/>
+            <center><b>Agenda de Contatos - Desenvolvida pela turma TDS06 - 2021</b></center>
+        </footer>
+    </body>
+</html>
